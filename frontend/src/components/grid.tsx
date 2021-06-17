aaf28c54d4a184b826a2825ed73932e0527fa8c2
Charles Paterson <cpat430@aucklanduni.ac.nz>
2021-06-17 16:21:16 +1200

    Added prettier

---
+++ frontend/src/components/grid.tsx
@@ -0,0 +1,12 @@
+type GridProps = {
+  rows: number;
+  cols: number;
+};
+
+// export const Grid = (props: GridProps) => {
+//     const { rows, cols } = props;
+// }
+
+export const Grid = (props: GridProps) => {
+  const { rows, cols } = props;
+};
